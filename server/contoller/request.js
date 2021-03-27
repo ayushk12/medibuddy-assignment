74de751ba92de0abcf49d1367b50294963bc438f
ayushsamata <ayush@samata.ui>
2021-03-27 21:27:17 +0530

    added request contoller

---
+++ server/contoller/request.js
@@ -0,0 +1,13 @@
+const Request = require('../models/requests');
+
+module.exports.set = (app) => {
+    app.post('/requests/create', (req, res) => {
+        var request = new Request(req.body);
+
+        request.save(req.body).then((data) => {
+            res.status(200).send(data);
+        }).catch((err) => {
+            res.status(400).send(err);
+        });
+    });
+}
