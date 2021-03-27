4201611e806b8a1d6c4c1626d6a88b14dff4c15a
ayushsamata <ayush@samata.ui>
2021-03-27 20:46:54 +0530

    added schema

---
+++ server/model/request.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const requestsSchema = new Schema({
+    request_id: String,
+    requester_id: String,
+    driver_assigned: {
+        type: String,
+        default: null
+    },
+    requested_at: {
+        type: Number,
+        default: new Date().getTime()
+    },
+    status: {
+        type: String,
+        default: 'Waiting'
+    }
+});
+
+requestsSchema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: function (doc, ret) {
+        ret.request_id = ret._id;
+        delete ret._id;
+        delete ret.id;
+    }
+});
+
+module.exports = mongoose.model('requests', requestsSchema);
