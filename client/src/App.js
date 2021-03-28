499e5ece3f391ca2dd67241dd29ae1d9494c0b37
ayushsamata <ayush@samata.ui>
2021-03-28 09:08:21 +0530

    added 3 tabs drivers

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Drivers from "./component/Drivers";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <div className="imageStyle">
+          <Switch>
+            <Route path="/" component={Drivers} />
+
+          </Switch>
+        </div>
+      </Router>
+    </React.Fragment>
   );
 }
 
